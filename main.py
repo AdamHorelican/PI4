948f1be97455d4a1e4636c64b205280bb72d5931
horelicana21 <horelicanadam@gmail.com>
2023-10-20 10:00:49 +0200

    Dedenie

--- main.py
+++ main.py
@@ -1,16 +1,99 @@
-# This is a sample Python script.
+import datetime
+import random
+class Osoba:
+    def __init__(self, meno, priezvisko, rok):
+        self.meno = meno
+        self.priezvisko = priezvisko
+        self.rok = rok
+        self.vek = datetime.date.today().year - self.rok
+    #konštruktor
+    def pozdrav(self):
+        print("Zdravím, ja som", self.meno, self.priezvisko, "a mám", self.vek, "rokov.")
+    #metoda pozdrav
+    def vypis_vek(self):
+        print(self.vek)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Ucitel(Osoba):
+    def __init__(self, meno, priezvisko, rok, titul, predmet, trieda):
+        Osoba.__init__(self, meno, priezvisko, rok)
+        self.titul = titul
+        self.predmet = predmet
+        self.trieda = trieda
+    def pozdrav(self):
+        print("Dobrý deň, som učiteľ", self.titul, self.meno, self.priezvisko, "a mám", self.vek, "rokov a učím predmet", self.predmet, "v triede", self.trieda)
 
+class Student(Osoba):
+    def __init__(self, meno, priezvisko, rok, trieda):
+        Osoba.__init__(self, meno, priezvisko, rok)
+        self.trieda = trieda
+    def pozdrav(self):
+        print("Ahoj, som", self.meno, self.priezvisko, "a mám", self.vek, "rokov a som žiakom", self.trieda, "triedy.")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#adam = Osoba("Adam", "Horalka", 2007)
+#adam.pozdrav()
+#matej = Ucitel("Matej", "Češčo", 2006, "Ing.", "PROGRAMKO")
+#matej.pozdrav()
+#peto = Student("Peter", "Tatarka", 2005, "3.AG")
+#peto.pozdrav()
 
+pocet_studentov = 4
+pocet_ucitelov = 3
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Hello World2')
+studenti = list()
+ucitelia = list()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in range(pocet_studentov):
+    with open("mena.txt", "r", encoding="utf-8") as t:
+        mena = tuple(t)
+    meno = random.choice(mena)
+    meno = meno[:-1]
+
+    with open("priezviska.txt", "r", encoding="utf-8") as p:
+        priezviska = tuple(p)
+    priezvisko = random.choice(priezviska)
+    priezvisko = priezvisko[:-1]
+
+    rok = random.randint(2005, 2008)
+
+    if rok == 2005:
+        trieda = "IV."
+    elif rok == 2006:
+        trieda = "III."
+    elif rok == 2007:
+        trieda = "II."
+    else:
+        trieda = "I."
+
+    trieda = trieda + random.choice(("A", "B", "C"))
+
+    studenti.append(Student(meno, priezvisko, rok, trieda))
+print("Študenti: ")
+for i in range(pocet_studentov):
+    print(i, studenti[i].meno, studenti[i].priezvisko, studenti[i].vek, studenti[i].rok, studenti[i].trieda)
+print("\n")
+studenti[1].pozdrav()
+print("========================\n")
+print("Učitelia: ")
+for i in range(pocet_ucitelov):
+    with open("mena.txt", "r", encoding="utf-8") as t:
+        mena = tuple(t)
+    meno = random.choice(mena)
+    meno = meno[:-1]
+
+    with open("priezviska.txt", "r", encoding="utf-8") as p:
+        priezviska = tuple(p)
+    priezvisko = random.choice(priezviska)
+    priezvisko = priezvisko[:-1]
+
+    rok = random.randint(1960, 2004)
+
+    titul = random.choice(("Ing.", "Mgr.", "Bc."))
+    predmet = random.choice(("Anj", "Mat", "Prx"))
+    trieda =  random.choice(("I.", "II.", "III.", "IV."))
+    trieda = trieda + random.choice(("A", "B", "C"))
+
+    ucitelia.append(Ucitel(meno, priezvisko, rok, titul, predmet, trieda))
+for i in range(pocet_ucitelov):
+    print(i, ucitelia[i].meno, ucitelia[i].priezvisko, ucitelia[i].vek, ucitelia[i].rok, ucitelia[i].titul,ucitelia[i].predmet, ucitelia[i].trieda)
+print("\n")
+ucitelia[1].pozdrav()
